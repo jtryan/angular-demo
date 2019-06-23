9f40deade4a11eff92ea1daf4483188656084bac
John Ryan <uijedi@gmail.com>
2019-06-22 21:36:13 -0400

    Initial commit

---
+++ src/main/java/com/jotryan/angulardemo/AngularDemoApplication.java
@@ -0,0 +1,30 @@
+package com.jotryan.angulardemo;
+
+import com.jotryan.angulardemo.model.User;
+import com.jotryan.angulardemo.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class AngularDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularDemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel", "Sammy").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ src/main/java/com/jotryan/angulardemo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.jotryan.angulardemo.repositories;
+
+import com.jotryan.angulardemo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> { }
